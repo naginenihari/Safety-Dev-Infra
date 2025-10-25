daf7a4b24d06faae90f566bf0a877f5694cda277
naginenihari <naginenihari@gmail.com>
2025-10-25 21:48:14 +0530

    Safety Project Infra creation Terraform

---
+++ 00-VPC/main.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+
+ source = "git::https://github.com/naginenihari/Terraform-Module-VPC.git?ref=main"
+ vpc_cidr=var.vpc_cidr
+ project_name=var.project_name
+ environment=var.environment
+ vpc_tags = var.vpc_tags
+
+ ##Internet Gateway Creation here we don't have any user entering details
+
+ ##Public Subnet details
+ public_subnet_cidrs = var.public_subnet_cidrs
+
+ ##Private Subnet details
+ private_subnet_cidrs = var.private_subnet_cidrs
+
+  ##Database Subnet details
+ database_subnet_cidrs = var.database_subnet_cidrs
+
+ is_peering_required = true
+}
+
+
+
+
