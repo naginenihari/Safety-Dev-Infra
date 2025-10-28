ea6952a432a37a766b35a75ad2696d83531711c1
naginenihari <naginenihari@gmail.com>
2025-10-28 16:56:58 +0530

    perameter file updated

--- 00-VPC/perameters.tf
+++ 00-VPC/perameters.tf
@@ -15,3 +15,9 @@ resource "aws_ssm_parameter" "private_subnet_ids" {
   type  = "StringList"
   value = join(",",module.vpc.private_subnet_ids)
 }
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/database_subnet_ids"
+  type  = "StringList"
+  value = join(",",module.vpc.database_subnet_ids)
+}
