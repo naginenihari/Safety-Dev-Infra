c2ba6f2adc54cf9fff67c15d332d5d0f492c8268
naginenihari <naginenihari@gmail.com>
2025-10-28 18:54:37 +0530

    database file updated

--- 40-Databases/main.tf
+++ 40-Databases/main.tf
@@ -33,7 +33,48 @@ provisioner "file" {
   provisioner "remote-exec" {
     inline = [ 
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh"
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh mongodb"
+     ]
+  }
+}
+
+resource "aws_instance" "redis" {
+    ami = local.ami_id
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [local.redis_sg_id]
+    subnet_id = local.database_subnet_id
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name ="${local.common_name_suffix}-redis"
+        }
+    )
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+  aws_instance.redis.id
+  ]
+
+  connection {
+  type="ssh"
+  user="ec2-user"
+  password="DevOps321"
+  host=aws_instance.redis.private_ip
+}
+
+##Terraform copy this file to redis server
+provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh redis"
      ]
   }
 }
