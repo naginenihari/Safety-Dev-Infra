c22f3e38a33f35d90501b6f3aa055277f216e7dc
naginenihari <naginenihari@gmail.com>
2025-10-31 20:58:19 +0530

    mysql rules are updated

--- 40-Databases/main.tf
+++ 40-Databases/main.tf
@@ -119,3 +119,43 @@ provisioner "file" {
      ]
   }
 }
+
+resource "aws_instance" "mysql" {
+    ami = local.ami_id
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [local.mysql_sg_id]
+    subnet_id = local.database_subnet_id
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name ="${local.common_name_suffix}-mysql"
+        }
+    )
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+  aws_instance.mysql.id
+  ]
+
+  connection {
+  type="ssh"
+  user="ec2-user"
+  password="DevOps321"
+  host=aws_instance.mysql.private_ip
+}
+
+##Terraform copy this file to mysql server
+provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql"
+     ]
+  }
+}
