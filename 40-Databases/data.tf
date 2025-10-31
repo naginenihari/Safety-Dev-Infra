2e6290a78fcedfae104809b0cadf2496b6f82f2d
naginenihari <naginenihari@gmail.com>
2025-10-31 20:50:18 +0530

    mysql rules are updated

--- 40-Databases/data.tf
+++ 40-Databases/data.tf
@@ -34,3 +34,7 @@ data "aws_ssm_parameter" "database_subnet_ids" {
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
   name  = "/${var.project_name}/${var.environment}/rabbitmq/sg_id"
 }
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/mysql/sg_id"
+}
