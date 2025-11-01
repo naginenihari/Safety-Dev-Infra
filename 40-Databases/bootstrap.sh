8e394628b4b6865a90a6ea2ec79b328c5a639f91
naginenihari <naginenihari@gmail.com>
2025-11-01 12:51:37 +0530

    Ansible deatils are updated

--- 40-Databases/bootstrap.sh
+++ 40-Databases/bootstrap.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
+# component=$1
+# dnf install ansible -y
+# ansible-pull -U https://github.com/naginenihari/Ansible_Roboshop_Roles-Terraform.git -e component=$component main.yaml
+# # git clone ansible-playbook
+# # cd ansible-playbook
+# # ansible-playbook -i inventory main.yaml
+
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/naginenihari/Ansible_Roboshop_Roles-Terraform.git -e component=$component main.yaml
-# git clone ansible-playbook
-# cd ansible-playbook
-# ansible-playbook -i inventory main.yaml
+REPO_URL=https://github.com/naginenihari/Ansible_Roboshop_Roles-Terraform.git
+REPO_DIR=/opt/safety/ansible
+ANSIBLE_DIR=Ansible_Roboshop_Roles-Terraform
+LOG_PATH=/var/log/safety/ansible.log
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/safety
+touch ansible.log
+
+cd $REPO_DIR
+
+#check if ansible repo is already cloned or not
+
+if [ -d $ANSIBLE_DIR ]; then
+   cd $ANSIBLE_DIR
+   git pull
+else
+   git clone $REPO_URL
+   cd $ANSIBLE_DIR
+fi
+
+ansible-playbook -e component=$component main.yaml
