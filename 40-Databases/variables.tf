c97274992b4998b25944c5d75f59777c2d555646
naginenihari <naginenihari@gmail.com>
2025-11-02 16:43:04 +0530

    database route53 records are updated

--- 40-Databases/variables.tf
+++ 40-Databases/variables.tf
@@ -5,3 +5,29 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "sg_names" {
+  default = [
+    #Databases
+    "mongodb","redis","mysql","rabbitmq",
+    #Backend
+    "catalogue","user","cart","shipping","payment",
+
+    #frontend
+    "frontend",
+     #Bastion
+    "bastion",
+    #Frontend Applcation Loadbalancer
+    "frontend_alb",
+    #Backend Application Loadbalancer
+    "backend_alb"
+    ]
+}
+
+variable "zone_id" {
+  default = "Z05202297D7QR4S9L2JB"
+}
+
+variable "domain_name" {
+  default = "naginenihariaws.store"
+}
