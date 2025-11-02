5952f0b24d165e0e8445372fac0b23417f0b630e
naginenihari <naginenihari@gmail.com>
2025-11-02 15:13:17 +0530

    updated bastion rules

---
+++ 60-Catalogue/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "catalogue" {
+    ami = local.ami_id
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [local.catalogue_sg_id]
+    subnet_id = local.private_subnet_id
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name ="${local.common_name_suffix}-catalogue"
+        }
+    )
+}
