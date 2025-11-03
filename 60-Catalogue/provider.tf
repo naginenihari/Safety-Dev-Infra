f86d146ad4fddec1225daa0c062ac7065a82bb4c
naginenihari <naginenihari@gmail.com>
2025-11-03 16:42:09 +0530

    Catalogue service is creating

--- 60-Catalogue/provider.tf
+++ 60-Catalogue/provider.tf
@@ -8,7 +8,7 @@ terraform {
   
   backend "s3" {
     bucket = "remote-state-aws-terraform"
-    key    = "safety-dev-catalogue-backend_ALB-remote-state-file"
+    key    = "safety-dev-catalogue-backend_alb-remote-state-file"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
