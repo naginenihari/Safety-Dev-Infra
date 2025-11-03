cc50fed02e7595886b4e7487106db0ab2b3aa0ff
naginenihari <naginenihari@gmail.com>
2025-11-03 19:12:30 +0530

    stopped catalogue instance deleted

--- 50-Backend-ALB/main.tf
+++ 50-Backend-ALB/main.tf
@@ -44,15 +44,5 @@ resource "aws_route53_record" "backend_alb" {
   }
 }
 
-resource "terraform_data" "catalogue_local" {
-  triggers_replace = [
-    aws_instance.catalogue.id
-  ]
 
-  depends_on = [ aws_autoscaling_policy.catalogue ]
-  provisioner "local-exec" {
-    command = "aws ec2 terminate-instances --instance_ids ${aws_instance.catalogue.id}"
-  }
-
-  }
   
