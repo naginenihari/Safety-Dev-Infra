70efa4e371a0510c7d576bbb568c5c5e52d5bc0a
naginenihari <naginenihari@gmail.com>
2025-11-03 17:04:42 +0530

    root value updared and revert

--- 20-Bastion/bastion.sh
+++ 20-Bastion/bastion.sh
@@ -6,3 +6,6 @@ sudo xfs_growfs /home
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+
+## we are revert backing attached volume from root ##
+# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
