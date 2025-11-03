34443e7dbe3217762b4f12d89b6255cb7b4ffbb5
naginenihari <naginenihari@gmail.com>
2025-11-03 17:37:37 +0530

    backend alb route53 record created

--- 50-Backend-ALB/main.tf
+++ 50-Backend-ALB/main.tf
@@ -31,3 +31,15 @@ resource "aws_lb_listener" "backend_alb" {
     }
   }
 }
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend_alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_elb.backend_alb.dns_name
+    zone_id                = aws_elb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
