e8dbed10df054c411cca0e842deb85df01c3c05a
naginenihari <naginenihari@gmail.com>
2025-11-10 18:21:47 +0530

    updated the catalogue template version

---
+++ 80-Frontend-ALB/main.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "backend_alb" {
+  name               = "${local.common_name_suffix}-backend-alb"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.backend_alb_sg_id]
+  subnets            = local.private_subnet_ids
+
+  enable_deletion_protection = true  ##Prevent accidental deletion from UI
+
+ tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project_name}-${var.environment}-backend-alb"
+        }
+    )
+}
+
+##Backend ALB listening on port number 80
+resource "aws_lb_listener" "backend_alb" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Hi, am from backend ALB HTTP Health Check"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend_alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+
+  
