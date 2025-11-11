b67f601fba925482b6183bbbb5c5b567d9d30e78
naginenihari <naginenihari@gmail.com>
2025-11-11 14:15:27 +0530

    Bastion code updated

--- 20-Bastion/bastion.sh
+++ 20-Bastion/bastion.sh
@@ -1,3 +1,15 @@
+# #!/bin/bash
+# sudo growpart /dev/xvda 4
+# sudo lvextend -L +30G /dev/mapper/RootVG-homeVol 
+# sudo xfs_growfs /home
+
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+# sudo yum -y install terraform
+
+# ## we are revert backing attached volume from root ##
+# # sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
+
 #!/bin/bash
 sudo growpart /dev/xvda 4
 sudo lvextend -L +30G /dev/mapper/RootVG-homeVol 
@@ -7,5 +19,10 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
 
-## we are revert backing attached volume from root ##
-# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
+# creating databases
+cd /home/ec2-user
+git clone https://github.com/daws-86s/roboshop-dev-infra.git
+chown ec2-user:ec2-user -R roboshop-dev-infra
+cd roboshop-dev-infra/40-databases
+terraform init
+terraform apply -auto-approve
