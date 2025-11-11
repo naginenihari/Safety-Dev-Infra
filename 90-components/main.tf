cd9862ad79809130b3ecddc3024df0e10407ad06
naginenihari <naginenihari@gmail.com>
2025-11-11 19:32:16 +0530

    Components are updated

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,14 @@
+# module "components" {
+#   source = "../../Terraform-Safety-component"
+#   component = var.component
+#   rule_priority = var.rule_priority
+# }
+
+
 module "components" {
-  source = "../../Terraform-Safety-component"
-  component = var.component
-  rule_priority = var.rule_priority
+  for_each = var.component
+  source = "git::https://github.com/naginenihariTerraform-Safety-component.git?ref=main"
+  component = each.key
+  rule_priority = each.value.var.rule_priority
 }
+
