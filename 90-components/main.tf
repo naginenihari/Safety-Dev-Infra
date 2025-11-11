6de0d71c1194f88ebbecbcf2659e42d701f9a356
naginenihari <naginenihari@gmail.com>
2025-11-11 19:41:45 +0530

    Components are updated

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -9,5 +9,5 @@ module "components" {
   for_each = var.components
   source = "git::https://github.com/naginenihari/Terraform-Safety-component.git?ref=main"
   component = each.key
-  rule_priority = each.value.var.rule_priority
+  rule_priority = each.value.rule_priority
 }
