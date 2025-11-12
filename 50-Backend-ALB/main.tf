5003da04d77f77da04f69da9666908061d32002d
naginenihari <naginenihari@gmail.com>
2025-11-12 18:10:12 +0530

    Backendalb code updated

--- 50-Backend-ALB/main.tf
+++ 50-Backend-ALB/main.tf
@@ -35,8 +35,9 @@ resource "aws_lb_listener" "backend_alb" {
 
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
-  name    = "*.backend_alb-${var.environment}.${var.domain_name}"
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
   type    = "A"
+  allow_overwrite = true
 
   alias {
     name                   = aws_lb.backend_alb.dns_name
