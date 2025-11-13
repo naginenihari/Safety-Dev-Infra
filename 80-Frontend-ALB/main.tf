19bead7ac1184f1dd9e64ffe7e691ffef32824fa
naginenihari <naginenihari@gmail.com>
2025-11-13 12:20:46 +0530

    Frontend ALB code updated

--- 80-Frontend-ALB/main.tf
+++ 80-Frontend-ALB/main.tf
@@ -3,6 +3,7 @@ resource "aws_lb" "frontend_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [local.frontend_alb_sg_id]
+  # it should be private subnet ids
   subnets            = local.public_subnet_ids
 
   enable_deletion_protection = false
@@ -19,7 +20,7 @@ resource "aws_lb" "frontend_alb" {
 
     resource "aws_lb_listener" "frontend_alb" {
       load_balancer_arn = aws_lb.frontend_alb.arn
-      port              = 443
+      port              = "443"
       protocol          = "HTTPS"
       ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06" # added a more recent policy
       certificate_arn   = local.frontend_alb_certificate_arn # ARN of your SSL certificate
@@ -43,6 +44,7 @@ resource "aws_lb" "frontend_alb" {
   allow_overwrite = true
 
   alias {
+  # These are ALB details, not our domain details
     name                   = aws_lb.frontend_alb.dns_name
     zone_id                = aws_lb.frontend_alb.zone_id
     evaluate_target_health = true
