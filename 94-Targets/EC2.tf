5418d50446f99255fdf2d161b91c3482e4eb473d
naginenihari <naginenihari@gmail.com>
2025-11-17 16:31:02 +0530

    Terraform target code created

---
+++ 94-Targets/EC2.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "terraform" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.lifecycle.id]
+  tags = {
+    Name      = "terraform"
+    Terraform = "true"
+  }
+
+}
+
+resource "aws_instance" "another" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "another"
+        Terraform = "true"
+    }
+    /* lifecycle{
+      prevent_destroy = true
+    } */
+}
+
+resource "aws_security_group" "lifecycle" {
+  name = "lifecycle---aaaa"
+
+  egress {
+    from_port   = 0 # from port 0 to to port 0 means all ports
+    to_port     = 0
+    protocol    = "-1"          # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] # internet
+  }
+
+  ingress {
+    from_port   = 0 # from port 0 to to port 0 means all ports
+    to_port     = 0
+    protocol    = "-1"          # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] # internet
+  }
+
+  tags = {
+    Name = "lifecycle"
+  }
+  lifecycle {
+    prevent_destroy = false  ## true
+}
+}
