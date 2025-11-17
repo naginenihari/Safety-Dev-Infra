72d2b08102a615509e915d4e4bf85056944f8e61
naginenihari <naginenihari@gmail.com>
2025-11-17 15:05:05 +0530

    Cloudfront code updated

--- 91-cloudfront/main.tf
+++ 91-cloudfront/main.tf
@@ -11,9 +11,9 @@ resource "aws_cloudfront_distribution" "safety" {
       origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
-enabled             = true
- #dev.naginenihariaws.store
- aliases = ["${var.environment}.${var.domain_name}"]
+  enabled = true
+  #dev.naginenihariaws.store
+  aliases = ["${var.environment}.${var.domain_name}"]
 
   # Cache behavior with precedence 2
   default_cache_behavior {
@@ -22,32 +22,29 @@ enabled             = true
     cached_methods   = ["GET", "HEAD"]
 
     viewer_protocol_policy = "https-only"
-    cache_policy_id = local.cachingDisabled
+    cache_policy_id        = local.cachingDisabled
   }
-
   # Cache behavior with precedence 0
-ordered_cache_behavior {
-    path_pattern     = "/media/*" 
+  ordered_cache_behavior {
+    path_pattern     = "/media/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = "${var.project_name}-${var.environment}.${var.domain_name}"
 
     viewer_protocol_policy = "https-only"
-    cache_policy_id = local.cachingOptimised
+    cache_policy_id        = local.cachingOptimised
 
-}
-
+  }
   # Cache behavior with precedence 1
-ordered_cache_behavior {
-    path_pattern     = "/images/*" 
-    allowed_methods = ["GET", "HEAD","OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
-    target_origin_id = "${var.project_name}-${var.environment}.${var.domain_name}"
+  ordered_cache_behavior {
+    path_pattern           = "/images/*"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "${var.project_name}-${var.environment}.${var.domain_name}"
     viewer_protocol_policy = "https-only"
-    cache_policy_id = local.cachingOptimised
+    cache_policy_id        = local.cachingOptimised
 
-}
-
+  }
   price_class = "PriceClass_All"
 
   restrictions {
@@ -57,21 +54,20 @@ ordered_cache_behavior {
     }
   }
   tags = merge(
-   local.common_tags,
-   {
-    Name = "${var.project_name}-${var.environment}"
-   }
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}"
+    }
   )
-    viewer_certificate {
+  viewer_certificate {
     acm_certificate_arn = local.cdn_certificate_arn
     ssl_support_method  = "sni-only"
   }
 }
-
 resource "aws_route53_record" "cdn" {
-  zone_id = var.zone_id
-  name    = "${var.environment}.${var.domain_name}" 
-  type    = "A"
+  zone_id         = var.zone_id
+  name            = "${var.environment}.${var.domain_name}"
+  type            = "A"
   allow_overwrite = true
 
   alias {
